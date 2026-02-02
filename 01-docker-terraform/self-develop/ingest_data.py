bb8bf472dcdb1fc65c44d755fc99a4a856204e7d
QuanLe <quanle504@gmail.com>
2026-02-03 02:00:55 +0700

    add file

--- 01-docker-terraform/self-develop/ingest_data.py
+++ 01-docker-terraform/self-develop/ingest_data.py
@@ -40,14 +40,14 @@ parse_dates = [
 @click.option('--pg_host', default='localhost', help='PostgreSQL host')
 @click.option('--pg_port', default=5432, type=int, help='PostgreSQL port')
 @click.option('--pg_db', default='ny_taxi', help='PostgreSQL database name')
-@click.option('--year', default=2021, type=int, help='Year of the data')
-@click.option('--month', default=1, type=int, help='Month of the data')
-@click.option('--target_table', default='yellow_taxi_data', help='Target table name')
+@click.option('--year', default=2025, type=int, help='Year of the data')
+@click.option('--month', default=11, type=int, help='Month of the data')
+@click.option('--target_table', default='green_taxi_data', help='Target table name')
 @click.option('--chunksize', default=100000, type=int, help='Chunk size for reading CSV')
 def run(pg_user, pg_pass, pg_host, pg_port, pg_db, year, month, target_table, chunksize):
     """Ingest NYC taxi data into PostgreSQL database."""
-    prefix = 'https://github.com/DataTalksClub/nyc-tlc-data/releases/download/yellow/'
-    url = prefix + f'yellow_tripdata_{year}-{month:02d}.csv.gz'
+    prefix = 'https://d37ci6vzurychx.cloudfront.net/trip-data/'
+    url = prefix + f'green_tripdata_{year}-{month:02d}.parquet'
 
     engine = create_engine(f'postgresql://{pg_user}:{pg_pass}@{pg_host}:{pg_port}/{pg_db}')
 
@@ -62,7 +62,7 @@ def run(pg_user, pg_pass, pg_host, pg_port, pg_db, year, month, target_table, ch
     for df_chunk in tqdm(df_iter):
         if first:
             df_chunk.head(0).to_sql(
-                name='yellow_taxi_data',
+                name='green_taxi_data',
                 con=engine,
                 if_exists='replace'
             )
